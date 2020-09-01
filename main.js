ae60742520dbb1d5d79e5d0443f29fd1273c01bd
Danilo Patanè <d.patane98@gmail.com>
2020-09-01 17:07:24 +0200

    done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var apri = $(".header-right a .fa-bars");
-var chiudi = $(".close .fa-times");
+var apri = $(".header-right a");
+var chiudi = $(".close");
 apri.click(
   function() {
     $('.hamburger-menu').addClass('active');
