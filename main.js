d34a976c189b685eb526ec128c4ac50012919d5b
Danilo Patanè <d.patane98@gmail.com>
2020-09-02 10:07:26 +0200

    Animation

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var apri = $(".header-right a");
 var chiudi = $(".close");
 apri.click(
   function() {
-    $('.hamburger-menu').addClass('active');
+    // $('.hamburger-menu').addClass('active');
+    $('.hamburger-menu').show('fast');
+
 
 }
 )
 chiudi.click(
   function() {
-    $('.hamburger-menu').toggleClass('active');
-
+    // $('.hamburger-menu').toggleClass('active');
+$('.hamburger-menu').hide('fast');
 }
 )
